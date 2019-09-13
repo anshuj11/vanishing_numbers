06ed126d6fc6f53cc74301fd27a4403de3533f5e
Anshuj <anshuj@gmail.com>
2019-09-13 09:38:27 -0700

    Initializing for Heroku push

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./frontend/vanishing_numbers.js",
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/env", "@babel/react"]
+          }
+        }
+      }
+    ]
+  },
+  devtool: "source-map"
+};
